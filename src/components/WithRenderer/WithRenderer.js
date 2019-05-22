98bddb51755b990059c38ac85b2516a789b7fa35
Jonathan Blair <jon@tothemun.com>
2019-05-22 17:07:19 -0400

    Removing dead observer code

--- src/components/WithRenderer/WithRenderer.js
+++ src/components/WithRenderer/WithRenderer.js
@@ -1,6 +1,4 @@
-import Observer from '@researchgate/react-intersection-observer';
 import React from 'react';
-import 'intersection-observer';
 import styles from './WithRenderer.css';
 
 function withRender(WrappedComponent) {
@@ -21,35 +19,16 @@ function withRender(WrappedComponent) {
       });
     };
 
-    handleIntersection = (event) => {
-      if(event.isIntersecting) {
-        this.setState({
-          pauseRender: false
-        });
-      } else {
-        this.setState({
-          pauseRender: true
-        });
-      }
-    }
-
     render() {
       const { height, pauseRender, width } = this.state;
 
-      const options = {
-        onChange: this.handleIntersection,
-        root: "#root"
-      };
-
       return (
-        <Observer {...options}>
-          <div
-            className={styles.wrapper}
-            ref={(el) => { this.$container = el; }}
-          >
-            <WrappedComponent height={height} width={width} pauseRender={pauseRender}/>
-          </div>
-        </Observer>
+        <div
+          className={styles.wrapper}
+          ref={(el) => { this.$container = el; }}
+        >
+          <WrappedComponent height={height} width={width} pauseRender={pauseRender}/>
+        </div>
       );
     }
 
