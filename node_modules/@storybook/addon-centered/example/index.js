6618935a2aed59bc49d5a914f918ddb9e5687340
Jonathan Blair <jon@tothemun.com>
2019-05-22 17:08:35 -0400

    Restoring missing gitignore

--- node_modules/@storybook/addon-centered/example/index.js
+++
@@ -1,7 +0,0 @@
-import React, { Component } from 'react';
-import ReactDOM from 'react-dom';
-import decorator from '../index';
-
-const content = decorator(() => 'Hello World!');
-const wrapper = document.querySelector('#content');
-ReactDOM.render(content, wrapper);
